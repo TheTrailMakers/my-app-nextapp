abd8716f9dcc0724129e4e28afa8a39dea080acf
Pritam-00 <pritambera11235@gmail.com>
2024-04-15 20:27:38 +0530

    add trail makers motto & next heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,12 +33,29 @@ const {hamburgerOpen} = useStore((state:any) => {
       <div className="h-40"></div>
       <h1 className="font-bold text-white text-center h-96"> Hello World</h1>
       <h3 className="font-bold text-yellow-500 text-3xl uppercase ml-8">Upcoming<br/>Treks...</h3>
-      <div className="flex overflow-x-auto no-scrollbar snap-x bg-black">
+      <div className="flex overflow-x-auto snap-x bg-black">
         <Homecards {...mockdata[0]}/>
         <Homecards {...mockdata[1]}/>
         <Homecards {...mockdata[2]}/>
         <Homecards {...mockdata[3]}/>
       </div>
+
+      <div className=" m-6 pt-10 pr-6 text-base font-medium text-neutral-500">
+        As people moved from agarian society to Industrial society, individuals started to become specialised in single persuits. Same stuff everyday broke the back, while as a human they aren’t meant to do so, they kept them alive, but never let live.
+        <br/><br/>
+        Capitalism prioritise WANT, not NEED. <strong>Human need Wide Open Space </strong>, Boundaryless Grassfield.
+        <br/><br/>
+        So today we have bunch of young people, who by mind and body are depresssed, frustrated, living inside a cubicle.
+        <br/><br/>
+        The Trail Makers works as a gateway to see the world as it is, inspire and encourage people to solve the knots of society in their own way.
+        <div className=" mx-auto pt-24 border-b-2 w-3/4 border-yellow-500"></div>
+      </div>
+
+      <div className="text-white text-7xl font-extrabold mx-4 my-20">
+        <h2 className="uppercase">How Trekking can change life</h2>
+      </div>
+
+      <div className="h-96"></div>
     </main>
   );
 }
