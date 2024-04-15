fac492db7931e272434a622be98c75e808369790
Pritam-00 <pritambera11235@gmail.com>
2024-04-15 18:22:47 +0530

    change button ui

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({text, bgcolor, txtcolor }:any) {
   return (
-    <button className={`${bgcolor} ${txtcolor} font-bold text-xs uppercase py-1 px-4 rounded-lg`}>{text}</button>
+    <button className={`${bgcolor} ${txtcolor} font-extrabold text-xs uppercase py-1 px-4 rounded-lg`}>{text}</button>
   )
 }
 
