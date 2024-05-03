ab5687f0c781e81a2b1b208d541b05d584e2ef81
Pritam-00 <pritambera11235@gmail.com>
2024-05-03 20:27:49 +0530

    add sitemap

---
+++ src/app/sitemap.tsx
@@ -0,0 +1,21 @@
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://thetrailmakers.in',
+      lastModified: new Date()
+    },
+
+    {
+      url: 'https://thetrailmakers.in/bhrigu-lake-trek',
+      lastModified: new Date()
+    },
+
+    {
+      url: 'https://thetrailmakers.in/contact',
+      lastModified: new Date()
+    }
+  ]
+}
+
+
+  
