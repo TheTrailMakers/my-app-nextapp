366e877e9533b3119f1d77265ef34c0fb51bd7ee
Pritam-00 <pritambera11235@gmail.com>
2024-07-05 10:36:18 +0530

    more ui optimisatio

--- src/components/galleryImage.tsx
+++ src/components/galleryImage.tsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
 
-function GalleryImage({Img, Name} : any) {
+function GalleryImage({ImageLinks} : any) {
 
   const default_Image = "https://res.cloudinary.com/thetrail/image/upload/v1714107209/default_trek_image.jpg"
+  
   return (
-    <div className='snap-center mx-2 w-auto h-full flex-shrink-0 '>
-
+    <div className='snap-center mx-2 h-96 flex-shrink-0'>
+      <div className='relative h-96'>
         <Image
-            src={Img || default_Image}
-            alt={Name}
-            height={320}
-            width={320}
-            className=' h-96 object-cover rounded-2xl'
-        >
-
-        </Image>
+          src={ImageLinks || default_Image}
+          alt="Beas Kund"
+          layout="resposive"
+          width={480}
+          height={480}
+          className='h-96 w-auto object-cover rounded-2xl'
+        />
+      </div>
     </div>
   )
 }
