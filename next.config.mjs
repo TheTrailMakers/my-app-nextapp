8c309eb2214f067f50b75e096255671bc8204783
Pritam-00 <pritambera11235@gmail.com>
2024-08-03 10:46:31 +0530

    meta change, image add

--- next.config.mjs
+++ next.config.mjs
@@ -38,6 +38,12 @@ const nextConfig = {
           pathname: '**',
         },
 
+        {
+          protocol: 'https',
+          hostname: 'images.ctfassets.net',
+          pathname: '**',
+        },
+
       ],
     },
 
